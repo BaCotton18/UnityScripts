b1e84e59f9c1798d7f59c8dd5415a3c526ee2589
BAcotton18 <bacotton@hotmail.com>
2022-04-29 14:11:18 -0400

    Initial commit
    The two scripts I made for triggering animations on Unity. SelfDestruct destroys the trigger after it plays once.

---
+++ PlayMyAnimation.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMyAnimation : MonoBehaviour
+{
+    [SerializeField] private Animator MyAnimationController;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            MyAnimationController.SetBool("PlayAnim", true);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            MyAnimationController.SetBool("PlayAnim", false);
+        }
+    }
+}
---
+++ selfDestruct.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class selfDestruct : MonoBehaviour
+{
+    [SerializeField] private GameObject self;
+    private bool entered = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            entered = true;
+            //Goodbye cruel world :(
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (entered = true)
+        {
+            Destroy(self);
+        }
+    }
+}
